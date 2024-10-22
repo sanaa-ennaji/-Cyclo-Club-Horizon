694b643837abde67fe04b6b2b7dfd62fe22e87d9
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-10-22 22:12:58 +0100

    update config class

--- src/main/java/org/sanaa/brif6/CCH/config/AppConfig.java
+++ src/main/java/org/sanaa/brif6/CCH/config/AppConfig.java
@@ -1,9 +1,6 @@
 package org.sanaa.brif6.CCH.config;
 
 
-import org.sanaa.brif6.CCH.service.Impl.cyclistService;
-import org.sanaa.brif6.CCH.service.cyclistServiceI;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -12,15 +9,5 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @ComponentScan(basePackages = "org.sanaa.brif6.CCH")
 public class AppConfig {
-
-    @Bean
-    public cyclistServiceI cyclistService (){
-        return (cyclistServiceI) new cyclistService();
-    }
-
-
-
-
-
 
 }
