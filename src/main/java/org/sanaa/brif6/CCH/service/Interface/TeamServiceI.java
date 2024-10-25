c25df595d80ad1b65ce623ae70a7b69cca84888b
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-10-25 22:52:21 +0100

    add interface service team

--- src/main/java/org/sanaa/brif6/CCH/service/Interface/TeamServiceI.java
+++ src/main/java/org/sanaa/brif6/CCH/service/Interface/TeamServiceI.java
@@ -1,4 +1,18 @@
 package org.sanaa.brif6.CCH.service.Interface;
 
+import org.sanaa.brif6.CCH.dto.Request.TeamRequestDTO;
+import org.sanaa.brif6.CCH.dto.Response.TeamResponseDTO;
+
+import java.util.List;
+
+
 public interface TeamServiceI {
+
+    TeamResponseDTO create (TeamRequestDTO requestDTO);
+    TeamResponseDTO  get (Long id);
+    List<TeamRequestDTO> getALL ();
+    TeamResponseDTO update (Long id , TeamRequestDTO requestDTO);
+    void  delete (Long id);
+
+
 }
