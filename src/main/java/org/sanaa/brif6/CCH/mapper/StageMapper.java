e31a28482cdde1798973e7be61db6ea20ff544f6
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-10-25 20:20:53 +0100

    add mappers

--- src/main/java/org/sanaa/brif6/CCH/mapper/StageMapper.java
+++ src/main/java/org/sanaa/brif6/CCH/mapper/StageMapper.java
@@ -1,4 +1,13 @@
 package org.sanaa.brif6.CCH.mapper;
 
+import org.mapstruct.Mapper;
+import org.sanaa.brif6.CCH.dto.Request.StageRequestDTO;
+import org.sanaa.brif6.CCH.dto.Response.StageResponseDTO;
+import org.sanaa.brif6.CCH.entity.Stage;
+
+@Mapper( componentModel =  "spring" )
 public interface StageMapper {
+
+    Stage toEntity(StageRequestDTO stageRequestDTO);
+    StageResponseDTO toResponseDTO(Stage stage);
 }
--- src/main/java/org/sanaa/brif6/CCH/mapper/TeamMapper.java
+++ src/main/java/org/sanaa/brif6/CCH/mapper/TeamMapper.java
@@ -1,4 +1,13 @@
 package org.sanaa.brif6.CCH.mapper;
 
+import org.mapstruct.Mapper;
+import org.sanaa.brif6.CCH.dto.Request.TeamRequestDTO;
+import org.sanaa.brif6.CCH.dto.Response.TeamResponseDTO;
+import org.sanaa.brif6.CCH.entity.Team;
+
+@Mapper( componentModel = "spring" )
 public interface TeamMapper {
+
+    Team toEntity (TeamRequestDTO teamRequestDTO);
+    TeamResponseDTO toResponseDTO (Team team);
 }
