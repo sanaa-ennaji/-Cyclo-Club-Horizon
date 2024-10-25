9924a9dea1ccda7cfa48bf5acf4ebeee6bafabad
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-10-25 22:43:54 +0100

    add service interface od stage

--- src/main/java/org/sanaa/brif6/CCH/service/Interface/StageServiceI.java
+++ src/main/java/org/sanaa/brif6/CCH/service/Interface/StageServiceI.java
@@ -1,4 +1,15 @@
 package org.sanaa.brif6.CCH.service.Interface;
 
+import org.sanaa.brif6.CCH.dto.Request.StageRequestDTO;
+import org.sanaa.brif6.CCH.dto.Response.StageResponseDTO;
+
+import java.util.List;
+
 public interface StageServiceI {
+
+    StageResponseDTO create (StageResponseDTO requestDTO);
+    StageResponseDTO get (Long id);
+    List<StageResponseDTO> getALL ();
+    StageResponseDTO update (Long id , StageRequestDTO requestDTO);
+    void delete (Long id);
 }
