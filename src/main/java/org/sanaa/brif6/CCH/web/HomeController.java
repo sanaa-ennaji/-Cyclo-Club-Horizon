594a75384567bcec39e37a36dc2c755f6f2d65cd
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-10-27 17:11:16 +0100

    problem resolved by changing the name of the war

---
+++ src/main/java/org/sanaa/brif6/CCH/web/HomeController.java
@@ -0,0 +1,12 @@
+package org.sanaa.brif6.CCH.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the CCH API!";
+    }
+}
